0c5fe906fbfb136b012f86f69ed2505f6be6e5c9
imanariii <imanariii>
2023-10-20 00:34:18 +0300

    2) Добавил видео на задний фон+немногостилей

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css'
+import video from './assets/preview.mp4'
+
+const width = 1280
+const height = 720
 
 function App() {
   return (
     <>
-
+        <video src={video} width={width} height={height} controls />
     </>
   )
 }
